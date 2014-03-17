bc568019dc1d13a872fb8a0f6a9e5ea693847b35
steveg8879 <sgembar1@gmail.com>
2014-03-17 09:57:11 -0400

    Update currency-app.js

--- currency-app.js
+++ currency-app.js
@@ -2,29 +2,44 @@
 var http = require('http');
 var express = require('express'); // Express is used for handling our API calls
 require('date-utils'); // Date-Utils is used to extend the Date() functionality
-var mongodb = require('mongodb');
-var server = new mongodb.Server("127.0.0.1", 27017, {});
+
+
+//=================== Mongo Config ============================
+
+var mongodb = require('mongodb'); // MongoDB is a client to connect to MongoDB
+var server = new mongodb.Server("127.0.0.1", 27017, {}); // Create connection to Mongo Service
 var APIrequests;
 new mongodb.Db('currencyApp', server, {w:1}).open(function (error, client) {
     if (error) throw error;
-    APIrequests = new mongodb.Collection(client, 'APIrequests');
+    APIrequests = new mongodb.Collection(client, 'APIrequests'); // Add collection to manage
 });
 
 
 //===================== Express Setup ==========================
 
 var app = express();
-app.use(express.bodyParser());
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
-app.post('/convertUSD', function(request, response){
+app.use(express.bodyParser()); // Used to parse the body of requests
+app.use(express.compress()); // Used for compression of responses
+app.use(express.static(__dirname + '/public')); // Used to serve static files from the /public directory
+
+
+//========================== API ===============================
+
+/*
+This section creates an API endpoint of 'convertUSD'. The API accepts a POST with JSON data that contains an amount (key) value.
+When a POST comes in a GET request is made to the openexchangerates.org API, which provides a JSON response of current exchange rates.
+The function then performs a calculation of (dollar amount posted * specified exchange rate) and returns the values in a JSON response.
+The function also writes a record of the request to a MongoDB DB instance called currencyApp in a collection called APIrequests. 
+ */
+
+app.post('/convertUSD', function(request, response){ // Define the API endpoint
     var data = '';
-    http.get("http://openexchangerates.org/api/latest.json?app_id=b190a168472845cd9091fb52395a968e", function(res) {
+    http.get("http://openexchangerates.org/api/latest.json?app_id=b190a168472845cd9091fb52395a968e", function(res) { // Begin request to the openexchangerates.org API
         res.setEncoding('utf8');
-        res.on('data', function (chunk) {
+        res.on('data', function (chunk) { // This function takes the chunked responses from the request and adds them to the 'data' string
             data += chunk;
         });
-        res.on('end', function() {
+        res.on('end', function() { // Once the request has ended the function will turn the data string into an object, write a record to the DB and perform the calculations to be provided in the JSON response
             var d = new Date();
             data = JSON.parse(data);
             var amount = request.body.amount;
@@ -36,4 +51,4 @@ app.post('/convertUSD', function(request, response){
         response.send("Got error: " + e.message);
     });
 });
-app.listen(8088);
+app.listen(8088); // This is the port that the web service is listening on
